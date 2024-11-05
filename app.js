099ba8676c3d3e0957e915c305d8afa1364ff5f2
27-aditya <kambleaditya946@gmail.com>
2024-11-06 00:18:24 +0530

    [code] Finished the routing for posts and comments

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
+
+const postRoutes = require('./routes/postRoutes');
+const commentRoutes = require('./routes/commentRoutes');
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+}));
 
 app.use(express.json());
 
@@ -10,3 +18,6 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.get('/', (req, res) => {
     res.status(200).json({message: "The server is running"});
 })
+
+app.use('/posts', postRoutes);
+app.use('/comments', commentRoutes);
